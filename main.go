383307ec76805f9d1856601bf2ecead0c3336790
dmtruong4697 <truonggduonggmadridista@gmail.com>
2026-01-06 22:00:38 +0700

    02

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "🚀 Hello from EC2 - CI/CD success!")
-	fmt.Fprintln(w, "DEPLOY: 01")
+	fmt.Fprintln(w, "DEPLOY: 02")
 }
 
 func main() {
